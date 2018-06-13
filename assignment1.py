6fa1ae9e04d19c508ef8c99f7087d9eb6f9deb6a
kavyakapoor0810 <kavyakapoor0810@gmail.com>
2018-06-13 21:58:38 +0530

    assignment1 done

--- assignment1.py
+++ assignment1.py
@@ -1,3 +1,36 @@
 #question1
 
-print ("hello")
+print ("hello my name is kavya")
+
+#question2
+
+a= "acad"
+b= "view"
+print (a+b)
+
+#question3
+
+x=input("enter name ")
+y=input("enter age ")
+z=input("enter phone no ")
+print(x)
+print(y)
+print(z)
+
+#question4
+
+print("let\'s get started")
+
+#question5
+
+s="acadview"
+course="python"
+fees=5000
+print("welcome to %s your course is %s and its fees is %d"%(s,course,fees))
+
+#question6
+
+name="Tony Stark"
+salary=1000000
+print('%s %d'%(name,salary))
+
