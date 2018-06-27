8de90f856b6a8ea843d148774caad8e99f5f06bf
kavyakapoor0810 <kavyakapoor0810@gmail.com>
2018-06-27 22:34:26 +0530

    12

--- assignment11.py
+++ assignment11.py
@@ -1,4 +1,4 @@
-#question1
+ #question1
 
 import threading
 from threading import Thread
@@ -10,7 +10,7 @@ class mythread(threading.Thread):
 
     def run(self):
         time.sleep(5)
-        print("Value after 5seconds")
+         print("Value after 5seconds")
 th=mythread()
 th.start()
 
@@ -19,7 +19,6 @@ th.start()
 class mythread1(threading.Thread):
     def __init__(self):
        threading.Thread.__init__(self)
-
     def run(self):
         for i in range(1,11):
             time.sleep(1)
@@ -30,30 +29,43 @@ thr.start()
 #question3
 
 class mythread3(threading.Thread):
-    def __init__(self):
-       threading.Thread.__init__(self)
-
+    l = [2, 4, 6, 8, 10]
+    def __init__(self,sleep):
+        threading.Thread.__init__(self)
+        self.sleep=sleep
     def run(self):
-        l = [1, 2, 3, 4, 5]
-        for i in l:
-            print(i)
-            r=[2,4,6,8,10]
-            for j in r:
-                time.sleep(j)
-
-thread=mythread3()
+        for i in range(1,5):
+            for j in self.l:
+                print(i)
+                time.sleep(1)
+thread=mythread3([1,2,3,4,5])
 thread.start()
 
 #question4
 
 import math
-
 class mythread2(threading.Thread):
     def __init__(self,i):
         threading.Thread.__init__(self)
-        self.i=i
+        self.a=i
     def run(self):
         print(math.factorial(self.i))
 i=int(input("enter no. for which u want to calc factorial"))
 thre=mythread2(i)
 thre.start()
+
+import time
+import threding
+class Mythread(threading.Thread):
+    def __init__(self,l,t):
+        self.l=l
+        self.t=t
+        threading.Thread.__init__(self)
+    def run(self):
+        for i,j in zip(l,t):
+            ptint(i)
+            time.sleep(j)
+l=[1,2,3,4,5]
+t=[2,4,6,8,10]
+MM=Mythread(l,t)
+MM.start()
