c228951a14552df0993b31affff7129d256e4faa
Kavya Kapoor <32263500+kavyakapoor0810@users.noreply.github.com>
2018-06-28 19:16:34 +0530

    12

--- assignment12.py
+++ assignment12.py
@@ -53,3 +53,17 @@ library is API.
 '''
 
 #question5
+
+import sendgrid
+import os
+from sendgrid.helpers.mail import *
+sg = sendgrid.SendGridAPIClient(apikey='')
+from_email = Email("kavyakapoor0810@gmail.com")
+to_email = Email("kavyakapoor0810@gmail.com")
+subject = "Sending with SendGrid is Fun"
+content = Content("text/plain", "and easy to do anywhere, even with Python")
+mail = Mail(from_email, subject, to_email, content)
+response = sg.client.mail.send.post(request_body=mail.get())
+print(response.status_code)
+print(response.body)
+print(response.headers)
