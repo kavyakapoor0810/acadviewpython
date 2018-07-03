dc939f2c47aeeb12616d9083ceda3792cad3c983
Kavya Kapoor <32263500+kavyakapoor0810@users.noreply.github.com>
2018-07-03 09:31:24 +0530

    16

--- assignment16.py
+++ assignment16.py
@@ -46,3 +46,46 @@ a.execute("INSERT INTO Authors_Titles(Author_Title_ID ,Author_ID ,Title_ID) VALU
 a.execute("INSERT INTO Authors(Authors_ID,First_Name,Middle_Name,Last_Name) VALUES('1','Miss','Kavya','Kapoor')")
 a.execute("INSERT INTO Authors(Authors_ID,First_Name,Middle_Name,Last_Name) VALUES('2','Miss','Kashish','Gupta')")
 a.commit()
+print("BEFORE UPDATION")
+b=a.execute("SELECT Book_ID,Title_ID,Location,Genre from Books")
+for B in b:
+    print("Book_ID:%s,\tTitle_ID:%s,\tLocation:%s,\tGenre:%s"%(B[0],B[1],B[2],B[3]))
+c=a.execute("SELECT Titles_ID,Title,ISBN,Publisher_ID,Publication_Year from Titles")
+for C in c:
+    print("Titles_ID:%s,Title:%s,ISBN:%s,Publisher_ID:%s,Publication_Year:%s"%(C[0],C[1],C[2],C[3],C[4]))
+d=a.execute("SELECT Publishers_ID, Name,Street_Address,Zip_Code_ID from Publishers")
+for D in d:
+    print("Publishers_ID:%s\t, Name:%s\t,Street_Address:%s\t,Zip_Code_ID:%s"%(D[0],D[1],D[2],D[3]))
+e=a.execute("SELECT Zip_Codes_ID,City,State,Zip_Code from Zip_Codes")
+for E in e:
+    print("Zip_Codes_ID:%s\t,City:%s\t,State:%s\t,Zip_Code:%s\t"%(E[0],E[1],E[2],E[3]))
+f=a.execute("SELECT Author_Title_ID ,Author_ID ,Title_ID from Authors_Titles")
+for F in f:
+    print("Author_Title_ID:%s\t,Author_ID:%s\t,Title_ID:%s"%(F[0],F[1],F[2]))
+g=a.execute("SELECT Authors_ID,First_Name,Middle_Name,Last_Name from Authors")
+for G in g:
+    print("Authors_ID:%s\t,First_Name:%s\t,Middle_Name:%s\t,Last_Name:%s"%(G[0],G[1],G[2],G[3]))
+
+a.execute("UPDATE Books set Title_ID=2 where Genre='ishika'")
+a.execute("UPDATE Authors set Authors_ID=2 where Middle_Name='Ishika'")
+a.commit()
+
+print("AFTER UPDATION")
+b=a.execute("SELECT Book_ID,Title_ID,Location,Genre from Books")
+for B in b:
+    print("Book_ID:%s,\tTitle_ID:%s,\tLocation:%s,\tGenre:%s"%(B[0],B[1],B[2],B[3]))
+c=a.execute("SELECT Titles_ID,Title,ISBN,Publisher_ID,Publication_Year from Titles")
+for C in c:
+    print("Titles_ID:%s,Title:%s,ISBN:%s,Publisher_ID:%s,Publication_Year:%s"%(C[0],C[1],C[2],C[3],C[4]))
+d=a.execute("SELECT Publishers_ID, Name,Street_Address,Zip_Code_ID from Publishers")
+for D in d:
+    print("Publishers_ID:%s\t, Name:%s\t,Street_Address:%s\t,Zip_Code_ID:%s"%(D[0],D[1],D[2],D[3]))
+e=a.execute("SELECT Zip_Codes_ID,City,State,Zip_Code from Zip_Codes")
+for E in e:
+    print("Zip_Codes_ID:%s\t,City:%s\t,State:%s\t,Zip_Code:%s\t"%(E[0],E[1],E[2],E[3]))
+f=a.execute("SELECT Author_Title_ID ,Author_ID ,Title_ID from Authors_Titles")
+for F in f:
+    print("Author_Title_ID:%s\t,Author_ID:%s\t,Title_ID:%s"%(F[0],F[1],F[2]))
+g=a.execute("SELECT Authors_ID,First_Name,Middle_Name,Last_Name from Authors")
+for G in g:
+    print("Authors_ID:%s\t,First_Name:%s\t,Middle_Name:%s\t,Last_Name:%s"%(G[0],G[1],G[2],G[3]))
